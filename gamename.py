2f63456ba6638341a290afbf98de28ed61557f3b
thewatcher <thewatcheriswatchinu@gmail.com>
2016-02-18 19:43:26 +0100

    more bug fixes

--- gamename.py
+++ gamename.py
@@ -9,8 +9,8 @@ client = discord.Client()
 client.login(email, password)
 @client.event
 def on_message(message):
-    if message.author.id = client.user.id:
-        if message.content.startswith("$game ")
+    if message.author.id == client.user.id:
+        if message.content.startswith("$game "):
             input_ = message.content
             input_.split(" ")[0]
             stuff = input_.split(" ")[1:]
@@ -20,3 +20,5 @@ def on_message(message):
             client.change_status(game=game)
             client.send_message(message.channel, client.user.name + "'s game has been set to " + gameName)
             print("game has been set to" + gameName)
+
+client.run()
